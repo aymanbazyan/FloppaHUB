0c8a1f9f5ce70ddce6488fe81625bd1819a4fd12
krabby <103147442+krabby932@users.noreply.github.com>
2023-02-13 16:04:24 +0200

    added navigation

--- MainPage/scripts/script.js
+++ MainPage/scripts/script.js
@@ -2,7 +2,22 @@
 const navBtn = document.querySelector(".nav");
 const navIcon = document.querySelector(".nav__button");
 const navMenu = document.querySelector(".menu");
+const navItem = document.querySelectorAll(".menu__list-item");
+
+const navIconBefore = window.getComputedStyle(navIcon, "::before");
+const navIconAfter = window.getComputedStyle(navIcon, "::after");
 
 navBtn.addEventListener("click", (event) => {
-  navMenu.classList.toggle("hidden");
+  // navMenu.classList.toggle("hidden");
+
+  navIcon.classList.toggle("nav__button-clicked");
+
+  navMenu.classList.toggle("translateX");
+});
+
+navItem.forEach((btn) => {
+  btn.addEventListener("click", (Event) => {
+    navMenu.classList.toggle("translateX");
+    navIcon.classList.toggle("nav__button-clicked");
+  });
 });
