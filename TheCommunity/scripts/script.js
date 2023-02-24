6d95d74f668779e02e6be0be7f8a6961b28f549f
Ayman Bazyan <103147442+aymanbazyan@users.noreply.github.com>
2023-02-24 16:24:47 +0200

    fixed some bugs

--- TheCommunity/scripts/script.js
+++ TheCommunity/scripts/script.js
@@ -2,13 +2,16 @@
 
 const user = document.querySelectorAll(".user");
 const chat = document.querySelector(".master__chat");
+const globalChat = document.querySelector(".master__chat__global");
 const defaultText = document.querySelector(".master__chat__default");
 
 const removeDefault = function () {
   chat.classList.remove("default");
   defaultText.classList.add("hidden");
+  globalChat.classList.toggle("hidden");
 };
 
 for (let i = 0; i < user.length; i++) {
   user[i].addEventListener("click", removeDefault);
 }
+/////////
